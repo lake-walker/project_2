d75c58fa14d296dc3f999b4953c62bce2d371baf
Lake Brant <lake.brant@du.edu>
2021-02-03 17:00:26 -0700

    working on adding polygons to map

--- app.js
+++ app.js
@@ -1,119 +1,140 @@
 
 // Map section of app creation
 // funcion to return marker size based on suicides
-function SuicideMarkerSize(score) {
-    return score * 10000;
-}
-function HappinessMarkerSize(happy) {
-    return happy * 10000;
-}
-function gdpMarkerSize(happy) {
-    return happy * 100000;
-}
-function hdiMarkerSize(happy) {
-    return happy * 100000;
-}
+// function SuicideMarkerSize(score) {
+//     return score * 10000;
+// }
+// function HappinessMarkerSize(happy) {
+//     return happy * 10000;
+// }
+// function gdpMarkerSize(happy) {
+//     return happy * 100000;
+// }
+// function hdiMarkerSize(happy) {
+//     return happy * 100000;
+// }
 
 
 
-// Define arrays to hold created country markers 
-var suicideMarkers = [];
-var happinessMarkers = [];
-var gdpMarkers = [];
-var hdiMarkers = [];
+// // Define arrays to hold created country markers 
+// var suicideMarkers = [];
+// var happinessMarkers = [];
+// var gdpMarkers = [];
+// var hdiMarkers = [];
 
 
-d3.csv('country_data.csv').then(function(data) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        suicideMarkers.push(
-            L.circle([data[i].latitude, data[i].longitude], {
-                stroke: false,
-                fillOpacity: 0.75,
-                color: 'red',
-                fillColor: 'red',
-                radius: SuicideMarkerSize(data[i].sui_per_100k_2015)
-            }).bindPopup("<h3>" + data[i].country +
-                "</h3><hr><p>" + data[i].sui_per_100k_2015 + "</p>")
-        );
+// d3.csv('country_data.csv').then(function(data) {
+//     console.log(data);
+//     for (var i = 0; i < data.length; i++) {
+//         suicideMarkers.push(
+//             L.circle([data[i].latitude, data[i].longitude], {
+//                 stroke: false,
+//                 fillOpacity: 0.75,
+//                 color: 'red',
+//                 fillColor: 'red',
+//                 radius: SuicideMarkerSize(data[i].sui_per_100k_2015)
+//             }).bindPopup("<h3>" + data[i].country +
+//                 "</h3><hr><p>" + data[i].sui_per_100k_2015 + "</p>")
+//         );
 
-        happinessMarkers.push(
-            L.circle([data[i].latitude, data[i].longitude], {
-                stroke: false,
-                fillOpacity: 0.75,
-                color: 'purple',
-                fillColor: 'purple',
-                radius: HappinessMarkerSize(data[i].happiness_score_2015)
-            }).bindPopup("<h3>" + data[i].country +
-                "</h3><hr><p>" + data[i].sui_per_100k_2015 + "</p>")
-        );
+//         happinessMarkers.push(
+//             L.circle([data[i].latitude, data[i].longitude], {
+//                 stroke: false,
+//                 fillOpacity: 0.75,
+//                 color: 'purple',
+//                 fillColor: 'purple',
+//                 radius: HappinessMarkerSize(data[i].happiness_score_2015)
+//             }).bindPopup("<h3>" + data[i].country +
+//                 "</h3><hr><p>" + data[i].sui_per_100k_2015 + "</p>")
+//         );
 
-        gdpMarkers.push(
-            L.circle([data[i].latitude, data[i].longitude], {
-                stroke: false,
-                fillOpacity: 0.75,
-                color: 'green',
-                fillColor: 'green',
-                radius: gdpMarkerSize(data[i].economy_gdp_per_capita_2015)
-            }).bindPopup("<h3>" + data[i].country +
-             "</h3><hr><p>" + 'Suicides per 100k: ' + data[i].sui_per_100k_2015 + "</p>")
-        );
+//         gdpMarkers.push(
+//             L.circle([data[i].latitude, data[i].longitude], {
+//                 stroke: false,
+//                 fillOpacity: 0.75,
+//                 color: 'green',
+//                 fillColor: 'green',
+//                 radius: gdpMarkerSize(data[i].economy_gdp_per_capita_2015)
+//             }).bindPopup("<h3>" + data[i].country +
+//              "</h3><hr><p>" + 'Suicides per 100k: ' + data[i].sui_per_100k_2015 + "</p>")
+//         );
 
-        hdiMarkers.push(
-            L.circle([data[i].latitude, data[i].longitude], {
-                stroke: false,
-                fillOpacity: 0.75,
-                color: 'yellow',
-                fillColor: 'yellow',
-                radius: hdiMarkerSize(data[i].human_development_index)
-            }).bindPopup("<h3>" + data[i].country +
-             "</h3><hr><p>" + 'Suicides per 100k: ' + data[i].sui_per_100k_2015 + "</p>")
-        );
-    }
-    // Streetmap Layer
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/light-v10",
-        accessToken: API_KEY
-    });
+//         hdiMarkers.push(
+//             L.circle([data[i].latitude, data[i].longitude], {
+//                 stroke: false,
+//                 fillOpacity: 0.75,
+//                 color: 'yellow',
+//                 fillColor: 'yellow',
+//                 radius: hdiMarkerSize(data[i].human_development_index)
+//             }).bindPopup("<h3>" + data[i].country +
+//              "</h3><hr><p>" + 'Suicides per 100k: ' + data[i].sui_per_100k_2015 + "</p>")
+//         );
+//     }
+//     // Streetmap Layer
+//     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//         tileSize: 512,
+//         maxZoom: 18,
+//         zoomOffset: -1,
+//         id: "mapbox/light-v10",
+//         accessToken: API_KEY
+//     });
   
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "dark-v10",
-        accessToken: API_KEY
-    });
+//     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//         maxZoom: 18,
+//         id: "dark-v10",
+//         accessToken: API_KEY
+//     });
   
-    // create seperate layer groups
-    var suicides = L.layerGroup(suicideMarkers);
-    var happiness = L.layerGroup(happinessMarkers);
-    var gdp = L.layerGroup(gdpMarkers);
-    var hdi = L.layerGroup(hdiMarkers);
+//     // create seperate layer groups
+//     var suicides = L.layerGroup(suicideMarkers);
+//     var happiness = L.layerGroup(happinessMarkers);
+//     var gdp = L.layerGroup(gdpMarkers);
+//     var hdi = L.layerGroup(hdiMarkers);
     
-    var baseMaps = {
-        'Street Map': streetmap,
-        'Dark Map': darkmap
-    };
+//     var baseMaps = {
+//         'Street Map': streetmap,
+//         'Dark Map': darkmap
+//     };
     
-    var overlayMaps = {
-        'Suicide Number': suicides,
-        'Happiness Score': happiness,
-        'GDP per Capita': gdp,
-        'Human Development Index': hdi
-    };
+//     var overlayMaps = {
+//         'Suicide Number': suicides,
+//         'Happiness Score': happiness,
+//         'GDP per Capita': gdp,
+//         'Human Development Index': hdi
+//     };
     
-    var myMap = L.map('map', {
-        center: [51.4934, 0.0098],
-        zoom: 2,
-        layers: [streetmap, suicides, happiness, gdp, hdi]
-    });
+//     var myMap = L.map('map', {
+//         center: [51.4934, 0.0098],
+//         zoom: 2,
+//         layers: [streetmap, suicides, happiness, gdp, hdi]
+//     });
     
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
+//     L.control.layers(baseMaps, overlayMaps, {
+//         collapsed: false
+//     }).addTo(myMap);
+// });
+
+var myMap = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 3
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+var link = 'countries.geojson';
+d3.json('countries.geojson', function(data){
+    console.log(data.geometry);
+    L.geoJson(data).addTo(myMap);
 });
 
 // Add a tile layer (the background map image) to our map
