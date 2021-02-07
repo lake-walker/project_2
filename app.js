598603efa7e670c1118e4f5b56035e3bb5063f6f
Lake Brant <lake.brant@du.edu>
2021-02-06 17:29:14 -0700

    trying to map csv data to geojson data

--- app.js
+++ app.js
@@ -116,6 +116,7 @@
 //     }).addTo(myMap);
 // });
 
+// Outlines for every country
 var myMap = L.map("map", {
     center: [40.7128, -74.0059],
     zoom: 3
@@ -131,144 +132,113 @@ var myMap = L.map("map", {
     accessToken: API_KEY
   }).addTo(myMap);
 
-var link = 'countries.geojson';
-d3.json('countries.geojson', function(data){
-    console.log(data.geometry);
-    L.geoJson(data).addTo(myMap);
-});
 
-// Add a tile layer (the background map image) to our map
-// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "mapbox/streets-v11",
-//   accessToken: API_KEY
+d3.json('countries.geojson', function (data) {
+  d3.csv('country_data.csv', function (csv) {
+    var world = data.features;
+    csv.forEach(function(d, i) {
+      world.forEach(function(e, j) {
+        if (d.country === e.properties.ADMIN) {
+          e.properties.sui_per_100k_2015 = d.sui_per_100k_2015 
+        }
+      })
+    })
+    
+  })
+  L.geoJson(data).addTo(myMap);
+})
+
+
+
+
+// d3.csv('country_data.csv').then(function(data){
+//   function getColor(d) {
+//     return d > 10 ? '#00FFFF' :
+//           d > 7 ? '#0000FF' :
+//           d > 5 ? '#9900e6' :
+//           d > 3 ? '#CCCCFF' :
+//           d > 1 ? '#E97451' :
+//           d > 0 ? '#800000' :
+//           '#00FF00';
+//   }
+// });
+
+function getColor(d) {
+  return d > 10 ? '#00FFFF' :
+        d > 7 ? '#0000FF' :
+        d > 5 ? '#9900e6' :
+        d > 3 ? '#CCCCFF' :
+        d > 1 ? '#E97451' :
+        d > 0 ? '#800000' :
+        'black';
+};
+
+var link = 'countries.geojson';
+
+
+// var sui_data = [];
+// d3.csv('country_data.csv').then(function(data){
+//   data.forEach(function(d) {
+//     d.happiness_score_2015 = +d.happiness_score_2015;
+//     d.sui_per_100k_2015 = +d.sui_per_100k_2015;
+//     d.economy_gdp_per_capita_2015 = +d.economy_gdp_per_capita_2015;
+//     d.human_development_index = +d.human_development_index;
+//   });
+//   // console.log(data[129].sui_per_100k_2015);
+  
+//   for (i=0; i<data.length; i++) {
+//     sui_data.push(data[i].sui_per_100k_2015);
+//   }
+//     console.log(getColor(sui_data));
+//     var mapStyle = {
+//       color: 'white',
+//       fillColor: sui_data.forEach,
+//       fillOpacity: 0.5,
+//       weight: 1.5
+//     };
+  
+//   d3.json('countries.geojson').then(function(geo){
+//     L.geoJson(geo,{
+//       style: mapStyle
+//     }).addTo(myMap);
+//   });
 // });
 
 
-// // Bubble chart with D3 selection
-// // Create and set the svg elements
-// function makeResponsive() {
-//     var svgArea = d3.select('body').select('svg');
-//     if (!svgArea.empty()) {
-//         svgArea.remove();
-//     }
 
-//     var svgWidth = 960;
-//     var svgHeight = 700;
 
-//     var margin = {
-//         top: 20,
-//         right: 40,
-//         bottom: 60,
-//         left: 50
+
+
+
+
+// var mapStyle = {
+//   color: 'white',
+//   fillColor: data.sui_per_100k_2015,
+//   fillOpacity: 0.5,
+//   weight: 1.5
+// };
+// var sui_data = [];
+// d3.json('countries.geojson').then(function(error, geo) {
+//   d3.csv('country_data.csv').then(function(data){
+//     for (i=0; i<data.length; i++) {
+//       sui_data.push(data[i].sui_per_100k_2015);
 //     };
+//     L.geoJson(geo,{
+//       style: function(d) {
+//         return {
+//           color: 'white',
+//           fillColor: getColor(sui_data)
+//         }
+//       }
+//     }).addTo(myMap);
+//   })
+// });
 
-//     var width = svgWidth - margin.left - margin.right;
-//     var height = svgHeight - margin.top - margin.bottom;
 
-//     var svg = d3.select('#scatter')
-//         .append('svg')
-//         .attr('width', svgWidth)
-//         .attr('height', svgHeight);
-
-//     var chartGroup = svg.append('g')
-//         .attr('transform', `translate(${margin.left}, ${margin.right})`);
-
-//     d3.csv('country_data.csv').then(function(data) {
-//         //parse data/cast as numbers
-//         data.forEach(d => {
-//             d.total_suicides_no = +d.total_suicides_no;
-//             d.happiness_score = +d.happiness_score;
-//         });
-
-//         // Create scale functions
-//         var xLinearScale = d3.scaleLinear()
-//             .domain([4, d3.max(data, d => d.happiness_score)])
-//             .range([0, width]);
-
-//         var yLinearScale = d3.scaleLinear()
-//             .domain([0, d3.max(data, d=> d.total_suicides_no)])
-//             .range([height, 0]);
-        
-//         // Create axis functions
-//         var bottomAxis = d3.axisBottom(xLinearScale);
-//         var leftAxis = d3.axisLeft(yLinearScale);
-
-//         // Append Axes to the chartGroup
-//         chartGroup.append('g')
-//             .attr('transform', `translate(0, ${height})`)
-//             .call(bottomAxis);
-        
-//         chartGroup.append('g')
-//             .call(leftAxis);
-
-//         // Create Circles
-//         var circlesGroup = chartGroup.selectAll('circle')
-//             .data(data)
-//             .enter()
-//             .append('circle')
-//             .attr('cx', d => xLinearScale(d.happiness_score))
-//             .attr('cy', d => yLinearScale(d.total_suicides_no))
-//             .attr('r', '10') // Could make circles different sizes based on other variables
-//             .attr('fill', 'blue')
-//             .attr('opacity', '.5');
-        
-//         // Create Axes labels
-//         chartGroup.append('text')
-//             .attr('transform', 'rotate(-90)')
-//             .attr('y', 0 - margin.left)
-//             .attr('x', 0 - (height/2))
-//             .attr('dy', '1em')
-//             .attr('class', 'axisText')
-//             .text('Total Suicides Number');
-        
-//         chartGroup.append('text')
-//             .attr('transform', `translate(${width / 2}, ${height + margin.top + 30})`)
-//             .attr('class', 'axisText')
-//             .text('Happiness Index');
-        
-//         // Append a div to the body to create tooltips
-//         var toolTip = d3.select('body').append('div').attr('class', 'tooltip');
-
-//         // var toolTip = d3.tip()
-//         //     .attr('class', 'tooltip')
-//         //     .offset([80,-60])
-//         //     .html(function(d) {
-//         //         return (`Country: <strong>${data[i].country}</strong>`);
-//         //     });
-        
-//         // chartGroup.call(toolTip);
-
-//         // circlesGroup.on('mouseover', function(d) {
-//         //     toolTip.show(d, this);
-//         // })
-//         //     .on('mouseout', function(d) {
-//         //         toolTip.hide(d);
-//         //     });
-
-//         // add an onmouseover event to display a tooltip
-//         circlesGroup.on('mouseover', function(d, i) {
-//             toolTip.style('display', 'block');
-//             toolTip.html(`Country: <strong>${data[i].country}</strong>`)
-//                 .style('left', d3.event.pageX + 'px')
-//                 .style('top', d3.event.pageY + 'px');
-//         })
-//             .on('mouseout', function() {
-//                 toolTip.style('display', 'none');
-//             });
-        
-        
-//     }).catch(function(error) {
-//         console.log(error);
-//     });
-// }
-
-// makeResponsive();
-
-// d3.select(svgArea).on('resize', makeResponsive);
+// d3.json('countries.geojson').then(function(data){
+//     console.log(data);
+//     L.geoJson(data).addTo(myMap);
+// });
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
--- app1.js
+++ app1.js
@@ -1,143 +1,4 @@
-// // The code for the chart is wrapped inside a function that
-// // automatically resizes the chart
-// function makeResponsive() {
-
-//     // if the SVG area isn't empty when the browser loads,
-//     // remove it and replace it with a resized version of the chart
-//     var svgArea = d3.select("body").select("svg");
-  
-//     // clear svg is not empty
-//     if (!svgArea.empty()) {
-//       svgArea.remove();
-//     }
-  
-//     // SVG wrapper dimensions are determined by the current width and
-//     // height of the browser window.
-//     var svgWidth = window.innerWidth;
-//     var svgHeight = window.innerHeight;
-  
-//     var margin = {
-//       top: 50,
-//       bottom: 50,
-//       right: 50,
-//       left: 50
-//     };
-  
-//     var height = svgHeight - margin.top - margin.bottom;
-//     var width = svgHeight - margin.left - margin.right;
-  
-//     // Append SVG element
-//     var svg = d3
-//       .select("#scatter")
-//       .append("svg")
-//       .attr("height", svgHeight)
-//       .attr("width", svgWidth);
-  
-//     // Append group element
-//     var chartGroup = svg.append("g")
-//       .attr("transform", `translate(${margin.left}, ${margin.top})`);
-  
-//     // Read CSV
-//     d3.csv("country_data.csv").then(function(countryData) {
-//         console.log(countryData);
-//       // create date parser
-//       var dateParser = d3.timeParse("%d-%b");
-  
-//       // parse data
-//       countryData.forEach(function(data) {
-//         data.happiness_score = +data.happiness_score_2015;
-//         data.total_suicides_no = +data.sui_per_100k_2015;
-//       });
-  
-//       // create scales
-//       var xLinearScale = d3.scaleLinear()
-//             .domain([1, 8])
-//             .range([0, width]);
-  
-//       var yLinearScale = d3.scaleLinear()
-//         .domain([0, 30])
-//         .range([height, 0]);
-  
-//       // create axes
-//       var xAxis = d3.axisBottom(xLinearScale);
-//       var yAxis = d3.axisLeft(yLinearScale).ticks(6);
-  
-//       // append axes
-//       chartGroup.append("g")
-//         .attr("transform", `translate(0, ${height})`)
-//         .call(xAxis);
-  
-//       chartGroup.append("g")
-//         .call(yAxis);
-  
-//       // line generator
-//     //   var line = d3.line()
-//     //     .x(d => xLinearScale(d.date))
-//     //     .y(d => yLinearScale(d.medals));
-  
-//       // append line
-//     //   chartGroup.append("path")
-//     //     .data([countryData])
-//     //     .attr("d", line)
-//     //     .attr("fill", "none")
-//     //     .attr("stroke", "red");
-  
-//       // append circles
-//       var circlesGroup = chartGroup.selectAll("circle")
-//         .data(countryData)
-//         .enter()
-//         .append("circle")
-//         .attr("cx", d => xLinearScale(d.happiness_score_2015))
-//         .attr("cy", d => yLinearScale(d.sui_per_100k_2015))
-//         .attr("r", "10")
-//         .attr("fill", "gold")
-//         .attr("stroke-width", "1")
-//         .attr("stroke", "black");
-  
-//       // date formatter to display dates nicely
-//       var dateFormatter = d3.timeFormat("%d-%b");
-  
-//       // Step 1: Append tooltip div
-//       var toolTip = d3.select('body').append('div')
-//         .attr('class', 'tooltip');
-  
-//       // Step 2: Create "mouseover" event listener to display tooltip
-//       circlesGroup.on('click', function(d) {
-//         toolTip.style('display', 'block');
-//         toolTip.html(`Country: <strong>${d.country}</strong>`)
-//           .style('left', d3.event.pageX + 'px')
-//           .style('top', d3.event.pageY + 'px');
-//       })
-  
-//       // Step 3: Create "mouseout" event listener to hide tooltip
-//         // .on('mouseout', function() {
-//         //   toolTip.style('display', 'none');
-//         // })
-  
-//     }).catch(function(error) {
-//       console.log(error);
-//     });
-//   }
-  
-//   // When the browser loads, makeResponsive() is called.
-//   makeResponsive();
-  
-//   // When the browser window is resized, makeResponsive() is called.
-//   d3.select(window).on("resize", makeResponsive);
-// d3.csv("country_data.csv").then(function(countryData) {
-//     d3.select("tbody")
-//     .selectAll("tr")
-//     .data(countryData)
-//     .enter()
-//     .append("tr")
-//     .html(function(d) {
-//         return `<td>${d.country}</td><td>${d.economy_gdp_per_capita_2015}</td><td>${d.sui_per_100k_2015}</td><td>${d.happiness_score_2015}</td>`;
-//     });
-// });
-
-
-
-
+// Bubble Chart
 var svgWidth = 2000;
 var svgHeight = 1000;
 
@@ -313,7 +174,7 @@ d3.csv("country_data.csv").then(function(countryData, err) {
     .attr("r", 10)
     .attr("fill", d => getColor(d.region))
     .attr('text', d => d.country)
-    .attr("opacity", ".5");
+    .attr("opacity", ".5");  
 
   // Create group for two x-axis labels
   var labelsGroup = chartGroup.append("g")
@@ -332,13 +193,14 @@ d3.csv("country_data.csv").then(function(countryData, err) {
     .attr("value", "economy_gdp_per_capita_2015") // value to grab for event listener
     .classed("inactive", true)
     .text("GDP per Capita (2015)");
+  
 
-    var hdiLabel = labelsGroup.append("text")
-        .attr("x", 0)
-        .attr("y", 60)
-        .attr("value", "human_development_index") // value to grab for event listener
-        .classed("inactive", true)
-        .text("Human Development Index");
+  var hdiLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 60)
+      .attr("value", "human_development_index") // value to grab for event listener
+      .classed("inactive", true)
+      .text("Human Development Index");
 
   // append y axis
   chartGroup.append("text")
@@ -360,6 +222,7 @@ d3.csv("country_data.csv").then(function(countryData, err) {
       .text(function(d){return d})
       .attr('text-anchor', 'left')
       .style('alignment-baseline', 'middle')
+
 
   // updateToolTip function above csv import
   var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
