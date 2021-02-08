de32c4719acefc477969e690d1095828de60c80e
Lake Brant <lake.brant@du.edu>
2021-02-08 16:05:57 -0700

    all maps now responsive

--- app.js
+++ app.js
@@ -1,120 +1,4 @@
-
-// // Map section of app creation
-// // funcion to return marker size based on suicides
-// // function SuicideMarkerSize(score) {
-// //     return score * 10000;
-// // }
-// // function HappinessMarkerSize(happy) {
-// //     return happy * 10000;
-// // }
-// // function gdpMarkerSize(happy) {
-// //     return happy * 100000;
-// // }
-// // function hdiMarkerSize(happy) {
-// //     return happy * 100000;
-// // }
-
-
-
-// // Define arrays to hold created country markers 
-// var suicideMarkers = [];
-// var happinessMarkers = [];
-// var gdpMarkers = [];
-// var hdiMarkers = [];
-
-
-// d3.csv('country_data.csv').then(function(data) {
-//     console.log(data);
-//     for (var i = 0; i < data.length; i++) {
-//         suicideMarkers.push(
-//             L.circle([data[i].latitude, data[i].longitude], {
-//                 stroke: false,
-//                 fillOpacity: 0.75,
-//                 color: 'red',
-//                 fillColor: 'red',
-//                 radius: SuicideMarkerSize(data[i].sui_per_100k_2015)
-//             }).bindPopup("<h3>" + data[i].country +
-//                 "</h3><hr><p>" + data[i].sui_per_100k_2015 + "</p>")
-//         );
-
-//         happinessMarkers.push(
-//             L.circle([data[i].latitude, data[i].longitude], {
-//                 stroke: false,
-//                 fillOpacity: 0.75,
-//                 color: 'purple',
-//                 fillColor: 'purple',
-//                 radius: HappinessMarkerSize(data[i].happiness_score_2015)
-//             }).bindPopup("<h3>" + data[i].country +
-//                 "</h3><hr><p>" + data[i].sui_per_100k_2015 + "</p>")
-//         );
-
-//         gdpMarkers.push(
-//             L.circle([data[i].latitude, data[i].longitude], {
-//                 stroke: false,
-//                 fillOpacity: 0.75,
-//                 color: 'green',
-//                 fillColor: 'green',
-//                 radius: gdpMarkerSize(data[i].economy_gdp_per_capita_2015)
-//             }).bindPopup("<h3>" + data[i].country +
-//              "</h3><hr><p>" + 'Suicides per 100k: ' + data[i].sui_per_100k_2015 + "</p>")
-//         );
-
-//         hdiMarkers.push(
-//             L.circle([data[i].latitude, data[i].longitude], {
-//                 stroke: false,
-//                 fillOpacity: 0.75,
-//                 color: 'yellow',
-//                 fillColor: 'yellow',
-//                 radius: hdiMarkerSize(data[i].human_development_index)
-//             }).bindPopup("<h3>" + data[i].country +
-//              "</h3><hr><p>" + 'Suicides per 100k: ' + data[i].sui_per_100k_2015 + "</p>")
-//         );
-//     }
-//     // Streetmap Layer
-//     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//         tileSize: 512,
-//         maxZoom: 18,
-//         zoomOffset: -1,
-//         id: "mapbox/light-v10",
-//         accessToken: API_KEY
-//     });
-  
-//     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//         maxZoom: 18,
-//         id: "dark-v10",
-//         accessToken: API_KEY
-//     });
-  
-//     // create seperate layer groups
-//     var suicides = L.layerGroup(suicideMarkers);
-//     var happiness = L.layerGroup(happinessMarkers);
-//     var gdp = L.layerGroup(gdpMarkers);
-//     var hdi = L.layerGroup(hdiMarkers);
-    
-//     var baseMaps = {
-//         'Street Map': streetmap,
-//         'Dark Map': darkmap
-//     };
-    
-//     var overlayMaps = {
-//         'Suicide Number': suicides,
-//         'Happiness Score': happiness,
-//         'GDP per Capita': gdp,
-//         'Human Development Index': hdi
-//     };
-    
-//     var myMap = L.map('map', {
-//         center: [51.4934, 0.0098],
-//         zoom: 2,
-//         layers: [streetmap, suicides, happiness, gdp, hdi]
-//     });
-    
-//     L.control.layers(baseMaps, overlayMaps, {
-//         collapsed: false
-//     }).addTo(myMap);
-// });
+// Map creation
 
 // Outlines for every country
 var myMap = L.map("map", {
@@ -156,7 +40,27 @@ d3.json('countries.geojson').then(function (data) {
           return {
             color: 'white',
             fillColor: getColor(feature.properties.sui_per_100k_2015)
-          }
+          };
+        },
+        onEachFeature: function(feature, layer) {
+          layer.on({
+            mouseover: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.9
+              });
+            },
+            mouseout: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.5
+              });
+            },
+            click: function(event) {
+              myMap.fitBounds(event.target.getBounds());
+            }
+          });
+          layer.bindPopup("<h1>" + feature.properties.ADMIN + "</h1> <hr> <h2>" + feature.properties.sui_per_100k_2015 + "</h2>");
         }
       })
 
@@ -164,17 +68,59 @@ d3.json('countries.geojson').then(function (data) {
         style: function(feature) {
           return {
             color: 'white',
-            fillColor: getColor(feature.properties.happiness_score_2015)
-          }
+            fillColor: getColor(feature.properties.happiness_score_2015),
+            fillOpacity: 0.5,
+            weight: 1.5
+          };
+        },
+        onEachFeature: function(feature, layer) {
+          layer.on({
+            mouseover: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.9
+              });
+            },
+            mouseout: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.5
+              });
+            },
+            click: function(event) {
+              myMap.fitBounds(event.target.getBounds());
+            }
+          });
+          layer.bindPopup("<h1>" + feature.properties.ADMIN + "</h1> <hr> <h2>" + feature.properties.sui_per_100k_2015 + "</h2>");
         }
-      })
+      });
 
       var gdp_map = L.geoJson(data, {
         style: function(feature) {
           return {
             color: 'white',
             fillColor: getColor(feature.properties.economy_gdp_per_capita_2015)
-          }
+          };
+        },
+        onEachFeature: function(feature, layer) {
+          layer.on({
+            mouseover: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.9
+              });
+            },
+            mouseout: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.5
+              });
+            },
+            click: function(event) {
+              myMap.fitBounds(event.target.getBounds());
+            }
+          });
+          layer.bindPopup("<h1>" + feature.properties.ADMIN + "</h1> <hr> <h2>" + feature.properties.sui_per_100k_2015 + "</h2>");
         }
       })
 
@@ -183,7 +129,27 @@ d3.json('countries.geojson').then(function (data) {
           return {
             color: 'white',
             fillColor: getColor(feature.properties.human_development_index)
-          }
+          };
+        },
+        onEachFeature: function(feature, layer) {
+          layer.on({
+            mouseover: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.9
+              });
+            },
+            mouseout: function(event) {
+              layer = event.target;
+              layer.setStyle({
+                fillOpacity: 0.5
+              });
+            },
+            click: function(event) {
+              myMap.fitBounds(event.target.getBounds());
+            }
+          });
+          layer.bindPopup("<h1>" + feature.properties.ADMIN + "</h1> <hr> <h2>" + feature.properties.sui_per_100k_2015 + "</h2>");
         }
       })
       var baseMaps = {
